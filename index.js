55cdddc77497779c4fa2dd5b0d44926dfbae0841
Estevan <estevan.rodriguez10@yahoo.com>
2019-02-15 08:25:25 -0800

    Added start and server to the knexfile and got the server to listen on port 8000

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
 // code away!
+
+const express = require('express');
+const server = express();
+const dbProjects = require('./data/helpers/projectModel');
+const dbActions = require('./data/helpers/actionModel')
+server.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(8000,()=>{
+    console.log('\n*** Running on port 8000 ***\n');
+})
